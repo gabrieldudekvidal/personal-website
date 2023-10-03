174a10c9adfccaea08e16a2342b359ceadbd0c59
Gabriel Vidal <73966053+gabrieldudekvidal@users.noreply.github.com>
2023-10-04 02:24:46 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Inter&family=Space+Mono:wght@700&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/e21f627216.js" crossorigin="anonymous"></script>
+  <title>Gabriel Vidal</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+  <main>
+    <div class="top-nav">
+      <div class="nav-container">
+        <a href="pages/about.php">about me</a>
+        <a href="pages/experience.php">my work</a>
+      </div>  
+    </div>
+
+    <div class="main-text">
+      <h1>Hello, <span class="light-text">I'm Gabriel</span><br> I type stuff on a <i class="fa-solid fa-desktop"></i>. <br><i class="fa-solid fa-location-pin"></i> based in Vancouver, BC <i class="fa-brands fa-canadian-maple-leaf"></i></h1>
+    </div>
+
+    <div class="description">
+      <p>Thanks for the visit <i class="fa-regular fa-face-smile"></i>.</p>
+    </div>
+
+    <div class="social-links">
+      <a href=""><i class="fa-brands fa-github-alt"></i></a>
+      <a href=""><i class="fa-brands fa-linkedin-in"></i></a>
+      <a href=""><i class="fa-solid fa-envelope"></i></a>
+    </div>
+  </main>
+  
+</body>
+</html>
---
+++ pages/about.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Inter&family=Space+Mono:wght@700&display=swap" rel="stylesheet">
+  <title>Gabriel Vidal</title>
+  <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+
+  <main>
+
+    <?php require_once "../includes/navbar.html" ?>
+
+    <div class="content">
+      <h1>About me</h1>
+      <p>👋 Hello, I'm a 26-year-old tech enthusiast who embarked on a journey into the world of technology
+         a few years back. I'm fueled by a deep passion and unwavering motivation for what I do—I'm the type of person who voraciously consumes
+          information to acquire new knowledge and skills. While I primarily identify as a developer, I also have a penchant for outdoor pursuits 
+          such as hiking, skateboarding, and hosting barbecues. Moreover, I cherish my indoor activities, including playing the bass, immersing myself
+           in books, and nurturing my artistic side through drawing. And last but not least, cats hold a special place in my heart as my favorite companions.</p>
+    </div>
+
+  </main>
+  
+</body>
+</html>
